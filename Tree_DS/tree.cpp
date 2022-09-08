01760dedb67dcbdf28a9689be0d5abfe783ca0b6
mohiuddin007 <mohiuddinmazumder94@gmail.com>
2022-09-08 12:31:03 +0600

    tree data structure

--- Tree_DS/tree.cpp
+++ Tree_DS/tree.cpp
@@ -14,12 +14,69 @@ public:
     }
 };
 
-int main(){
-    treeNode* n1 = new treeNode(0);
-    treeNode* n2 = new treeNode(1);
+void printTree(treeNode* root, int level);
+void spacePint(int level);
 
-    n1->leftChild=n2;
+void printTree(treeNode* root, int level){
+    if(root == NULL){ //when the tree is empty
+        return;
+    } 
+    if(root->leftChild == NULL && root->rightChild == NULL){ 
+        cout<<root->data <<endl;
+        return;
+    } else {
+        cout<<endl;
+        spacePint(level);
+        cout<< "Root: " << root->data <<endl;
+    }
+
+    if(root->leftChild != NULL){
+        spacePint(level);
+        cout<< "Left: ";
+        printTree(root->leftChild, level+1);
+    }
+
+    if(root->rightChild != NULL){
+        spacePint(level);
+        cout<< "Right: ";
+        printTree(root->rightChild, level+1);
+    }
+}
+
+void spacePint(int level){
+    for(int i=0; i<level; i++){
+        cout<< "    ";
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    treeNode* allNodes[n];
     
+    for(int i=0; i<n; i++){
+        allNodes[i] = new treeNode(-1);
+    }
+
+    for(int i=0; i<n; i++){
+        int value, left, right;
+        cin>>value >> left >> right;
+        allNodes[i]->data = value;
+        if(left>n-1 || right > n-1){
+            cout<<"invalid index"<<endl;
+            break;
+        }
+        if(left != -1){
+            allNodes[i]->leftChild=allNodes[left];
+        }
+
+        if(right != -1){
+            allNodes[i]->rightChild=allNodes[right];
+        }
+    }
+
+    printTree(allNodes[0], 0);
+
     return 0;
 }
 
