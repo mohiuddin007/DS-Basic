f7eb4a279f7fdd718f0e51fe7fc63e09d1f3b712
mohiuddin007 <mohiuddinmazumder94@gmail.com>
2022-09-27 00:00:30 +0600

    hashing

---
+++ hashing/hashing.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    unordered_map<int, int> M;
+
+    M[3] = 9;
+    M[2] = 3;
+    M.insert(make_pair(1, 4));
+
+    cout<< "key" << " | " << "value" << endl;
+    for(auto i: M){
+        cout << i.first << " | " << i.second << endl;
+    }
+}
